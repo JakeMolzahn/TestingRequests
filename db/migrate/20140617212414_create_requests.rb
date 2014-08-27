acbf3e8858dae7b948bb1ea483eaa7107ff6be20
JakeMolzahn <jakemolzahn@gmail.com>
2014-08-27 13:59:15 -0400

    request fields
    set up initial requests fields in db

--- db/migrate/20140617212414_create_requests.rb
+++ db/migrate/20140617212414_create_requests.rb
@@ -5,6 +5,14 @@ class CreateRequests < ActiveRecord::Migration
       t.string :title
       t.text :description
       t.datetime :date
+      t.string :jira
+      t.string :stage
+      t.string :islive
+      t.datetime :startdate
+      t.datetime :enddate
+      t.text :status
+      t.text :testdata
+      t.string :creator
 
       t.timestamps
     end
